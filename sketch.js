f8c150acace491ed0c6624ff86927e8b086b0e36
guzinhoparente <00001114419205sp@al.educacao.sp.gov.br>
2024-08-12 17:06:16 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let cor;
+let vertical;
+let horizontal;
+
+function setup() {
+  createCanvas(400, 400);
+   background("rgb(228,50,50)");
+  cor=color(random(0,255),random(0,255),random(0,255));
+  
+  horizontal = 200;
+  vertical = 200;
+}
+  
+function draw() {
+  circle(horizontal, vertical, 35)
+   fill(cor);
+  
+  if (mouseX < horizontal) {
+  horizontal--
+  }
+  if (mouseX > horizontal) {
+  horizontal++
+  }
+  
+  if (mouseY < vertical) {
+  vertical--
+  }
+  if (mouseY > vertical) {
+  vertical++
+  }
+  
+  if(mouseIsPressed) {
+    
+    cor=color(random(0,255),random(0,255),random(0,255),random(0,100));
+    
+ }
+  
+
+}
